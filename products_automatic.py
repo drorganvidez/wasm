e8fb118e760473bf77381f6ae8960c92da3c10b4
José A. Galindo <jagalindo@us.es>
2022-08-19 18:38:42 +0200

    adding a simple python script

--- products_automatic.py
+++ products_automatic.py
@@ -1,4 +1,4 @@
 from flamapy.core.discover import DiscoverMetamodels # This loads the tool in the python execution environment
 dm = DiscoverMetamodels() # Instantiate the class
-result = dm.use_operation_from_file("Products", "./models/pizzas/pizzas.fide") # This launch the operation and stores the result on the result variable
+result = dm.use_operation_from_file("Products", "./tutorial-splc22/models/pizzas/pizzas.fide") # This launch the operation and stores the result on the result variable
 print(result) # to print the result
